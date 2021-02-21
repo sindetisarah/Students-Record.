687f03b87bb3e0fd130542f745c245546740a359
sindetisarah <sindetisara@gmail.com>
2021-02-21 11:40:48 +0300

    created anew file student records

---
+++ src/student records.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val secondName : String = "sindet"
+    val fullName : String = "sarah" + secondName
+    println(fullName)
+
+    var age: Int = 28
+    println(age)
+
+    var phoneNumber: String ="0721406307"
+    println(phoneNumber)
+
+    var weight:Double = 50.5
+    println(weight)
+
+    var citizen:Boolean = true
+    print(citizen)
+
+
+
+}
+
+
